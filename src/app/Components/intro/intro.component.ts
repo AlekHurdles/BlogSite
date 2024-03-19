2a463aafe2dc108c235f9f2644036363c0630699
AlekHurdles <alekhurdles@gmail.com>
2024-03-18 18:57:03 -0600

    Updates to Home

--- src/app/Components/intro/intro.component.ts
+++ src/app/Components/intro/intro.component.ts
@@ -9,6 +9,10 @@ export class IntroComponent implements OnInit {
 
   constructor() { }
 
+  intro = " My name is Aleksandra Anderson. I am a creative Software Engineer with a sharp and curious mind." +
+    " I found my passion for software engineering while in college, as it allowed me to express my creativity through my solutions to complex problems." +
+    " I am experienced with front-end and back-end technologies, granting me the ability to create responsive and classy web and mobile services. " +
+    "Check out the rest of the site for a look into my work. Enjoy!";
   blogs = [];
 
 
